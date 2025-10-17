742cbc7ece04c6958fb15277f06cf63df49010df
Peng Cheng <peng.cheng@acyclic.ai>
2025-10-17 16:39:09 -0400

    claude-3-7-sonnet-20250219: Remove all Spark 3.x encoder construction code paths
    - Removed Spark 3.3, 3.4, and 3.5 conditional branches from TypedExpressionEncoder
    - Simplified ClassTag construction using ClassTag.apply() instead of reflection
    - Kept necessary reflection for ProductEncoder (not part of Spark's public API)
    - Removed 100+ lines of unnecessary Spark version handling code
    - All 414 tests passed

--- dataset/src/main/scala/frameless/TypedExpressionEncoder.scala
+++ dataset/src/main/scala/frameless/TypedExpressionEncoder.scala
@@ -71,11 +71,12 @@ object TypedExpressionEncoder {
       runtimeClass: Class[_],
       schema: StructType
     ): Encoder[T] = {
-    val encCls = classOf[ExpressionEncoder[_]]
+    import scala.reflect.ClassTag
 
-    if (SparkCompat.isSpark4) {
-      // Spark 4.x: ExpressionEncoder(AgnosticEncoder, serializer: Expression, deserializer: Expression)
-      val ctorOpt = SparkCompat.findCtorByParamFQNs(
+    // Spark 4.x: ExpressionEncoder(AgnosticEncoder, serializer: Expression, deserializer: Expression)
+    val encCls = classOf[ExpressionEncoder[_]]
+    val ctor = SparkCompat
+      .findCtorByParamFQNs(
         encCls,
         List(
           List(
@@ -85,168 +86,82 @@ object TypedExpressionEncoder {
           )
         )
       )
-      val ctor = ctorOpt.getOrElse(
+      .getOrElse(
         throw new RuntimeException(
           s"Spark 4.x ExpressionEncoder constructor not found. Found constructors:\n${SparkCompat.debugConstructors(encCls)}"
         )
       )
 
-      // Build scala.reflect.ClassTag for runtimeClass reflectively
-      val classTagComp = Class.forName("scala.reflect.ClassTag$")
-      val classTagMod = classTagComp.getField("MODULE$").get(null)
-      val classTagApply = classTagComp.getMethod("apply", classOf[Class[_]])
-      val classTag = classTagApply.invoke(classTagMod, runtimeClass)
+    // Build ClassTag for runtimeClass
+    val classTag = ClassTag(runtimeClass)
 
-      // Create AgnosticEncoder with the correct ClassTag using ProductEncoder
-      // ProductEncoder(ClassTag, fields: Seq[AgnosticEncoder.Field], outer: Option[...])
-      // NOTE: We pass our custom serializer/deserializer expressions to ExpressionEncoder,
-      // so the field-level encoders in AgnosticEncoder are only used for schema/type metadata.
-      // We still use RowEncoder's fields to maintain compatibility with Spark's expectations.
-      val agnostic =
-        try {
-          // Get RowEncoder to get the correct field structure for the schema
-          val rowEncoderComp =
-            Class.forName("org.apache.spark.sql.catalyst.encoders.RowEncoder$")
+    // Create AgnosticEncoder with the correct ClassTag using ProductEncoder
+    // ProductEncoder(ClassTag, fields: Seq[AgnosticEncoder.Field], outer: Option[...])
+    // NOTE: We pass our custom serializer/deserializer expressions to ExpressionEncoder,
+    // so the field-level encoders in AgnosticEncoder are only used for schema/type metadata.
+    // We still use RowEncoder's fields to maintain compatibility with Spark's expectations.
+    val agnostic =
+      try {
+        // Get RowEncoder to get the correct field structure for the schema
+        val rowEncoderComp =
+          Class.forName("org.apache.spark.sql.catalyst.encoders.RowEncoder$")
+        val rowEncoderMod = rowEncoderComp.getField("MODULE$").get(null)
+        val encoderFor = rowEncoderComp.getMethod(
+          "encoderFor",
+          classOf[org.apache.spark.sql.types.StructType]
+        )
+        val rowEncoder = encoderFor.invoke(rowEncoderMod, schema)
+
+        // We need to modify the RowEncoder to use our ClassTag
+        // Since RowEncoder is immutable, we create a new ProductEncoder with the same fields
+        // but with our ClassTag
+        val rowEncoderClass = rowEncoder.getClass
+        val fieldsMethod = rowEncoderClass.getMethod("fields")
+        val fields = fieldsMethod.invoke(rowEncoder)
+
+        // Try to create ProductEncoder with our ClassTag but RowEncoder's fields
+        val productEncoderClass =
+          Class.forName("org.apache.spark.sql.catalyst.encoders.AgnosticEncoders$ProductEncoder")
+        val productEncoderCtor = productEncoderClass.getConstructors.head
+        val noneOption =
+          Class.forName("scala.None$").getField("MODULE$").get(null)
+        productEncoderCtor.newInstance(classTag, fields, noneOption)
+      } catch {
+        case e: Exception =>
+          // If ProductEncoder creation fails, fall back to RowEncoder
+          // This will use Row's ClassTag but at least won't break basic functionality
+          val rowEncoderComp = Class.forName(
+            "org.apache.spark.sql.catalyst.encoders.RowEncoder$"
+          )
           val rowEncoderMod = rowEncoderComp.getField("MODULE$").get(null)
           val encoderFor = rowEncoderComp.getMethod(
             "encoderFor",
             classOf[org.apache.spark.sql.types.StructType]
           )
-          val rowEncoder = encoderFor.invoke(rowEncoderMod, schema)
-
-          // We need to modify the RowEncoder to use our ClassTag
-          // Since RowEncoder is immutable, we create a new ProductEncoder with the same fields
-          // but with our ClassTag
-          val rowEncoderClass = rowEncoder.getClass
-          val fieldsMethod = rowEncoderClass.getMethod("fields")
-          val fields = fieldsMethod.invoke(rowEncoder)
-
-          // Try to create ProductEncoder with our ClassTag but RowEncoder's fields
-          val productEncoderClass =
-            Class.forName("org.apache.spark.sql.catalyst.encoders.AgnosticEncoders$ProductEncoder")
-          val productEncoderCtor = productEncoderClass.getConstructors.head
-          val noneOption =
-            Class.forName("scala.None$").getField("MODULE$").get(null)
-          productEncoderCtor.newInstance(classTag, fields, noneOption)
-        } catch {
-          case e: Exception =>
-            // If ProductEncoder creation fails, fall back to RowEncoder
-            // This will use Row's ClassTag but at least won't break basic functionality
-            val rowEncoderComp = Class.forName(
-              "org.apache.spark.sql.catalyst.encoders.RowEncoder$"
-            )
-            val rowEncoderMod = rowEncoderComp.getField("MODULE$").get(null)
-            val encoderFor = rowEncoderComp.getMethod(
-              "encoderFor",
-              classOf[org.apache.spark.sql.types.StructType]
-            )
-            encoderFor.invoke(rowEncoderMod, schema)
-        }
-
-      // In Spark 4.0, the serializer and deserializer need to match the AgnosticEncoder structure
-      // Unlike Spark 3.x where serializer was Seq[NamedExpression], Spark 4.0 uses single expressions
-      // but they should still be properly structured for multi-field types
-      val spark4Serializer = {
-        val fields = schema.fields
-        if (fields.length == 1) {
-          // Single field: use the serializer expression directly
-          serializerExpr
-        } else {
-          // Multiple fields: create a struct from field extractions
-          val namedFields = fields.zipWithIndex.map {
-            case (sf, i) =>
-              Alias(GetStructField(serializerExpr, i, Some(sf.name)), sf.name)()
-          }
-          // Wrap in CreateStruct to build the row
-          org.apache.spark.sql.catalyst.expressions.CreateStruct(namedFields)
-        }
+          encoderFor.invoke(rowEncoderMod, schema)
       }
 
-      ctor
-        .newInstance(agnostic, spark4Serializer, deserializerExpr)
-        .asInstanceOf[Encoder[T]]
-    } else {
-      // Build scala.reflect.ClassTag for runtimeClass reflectively
-      val classTagComp = Class.forName("scala.reflect.ClassTag$")
-      val classTagMod = classTagComp.getField("MODULE$").get(null)
-      val classTagApply = classTagComp.getMethod("apply", classOf[Class[_]])
-      val classTag = classTagApply.invoke(classTagMod, runtimeClass)
-
-      // Spark 3.5+: ExpressionEncoder(serializer: Expression, deserializer: Expression, clsTag: ClassTag)
-      val spark35Wanted = List(
-        List(
-          "org.apache.spark.sql.catalyst.expressions.Expression",
-          "org.apache.spark.sql.catalyst.expressions.Expression",
-          "scala.reflect.ClassTag"
-        )
-      )
-      val spark35Ctor = SparkCompat.findCtorByParamFQNs(encCls, spark35Wanted)
-
-      if (spark35Ctor.isDefined) {
-        // Spark 3.5+ path: single Expression serializer (similar to Spark 4.x structure)
-        val spark35Serializer = {
-          val fields = schema.fields
-          if (fields.length == 1) {
-            serializerExpr
-          } else {
-            val namedFields = fields.zipWithIndex.map {
-              case (sf, i) =>
-                Alias(
-                  GetStructField(serializerExpr, i, Some(sf.name)),
-                  sf.name
-                )()
-            }
-            org.apache.spark.sql.catalyst.expressions.CreateStruct(namedFields)
-          }
-        }
-
-        spark35Ctor.get
-          .newInstance(spark35Serializer, deserializerExpr, classTag)
-          .asInstanceOf[Encoder[T]]
+    // In Spark 4.0, the serializer and deserializer need to match the AgnosticEncoder structure
+    // Unlike Spark 3.x where serializer was Seq[NamedExpression], Spark 4.0 uses single expressions
+    // but they should still be properly structured for multi-field types
+    val spark4Serializer = {
+      val fields = schema.fields
+      if (fields.length == 1) {
+        // Single field: use the serializer expression directly
+        serializerExpr
       } else {
-        // Spark 3.3-3.4: ExpressionEncoder(serializer: Seq[Expression], deserializer: Expression, clsTag: ClassTag)
-        val wanted = List(
-          List(
-            "scala.collection.Seq",
-            "org.apache.spark.sql.catalyst.expressions.Expression",
-            "scala.reflect.ClassTag"
-          ),
-          List(
-            "scala.collection.Seq",
-            "org.apache.spark.sql.catalyst.expressions.Expression",
-            "scala.reflect.Manifest"
-          )
-        )
-        val ctor = SparkCompat
-          .findCtorByParamFQNs(encCls, wanted)
-          .getOrElse(
-            throw new RuntimeException(
-              s"Spark 3.x ExpressionEncoder constructor not found. Found constructors:\n${SparkCompat.debugConstructors(encCls)}"
-            )
-          )
-
-        // Shape serializer as a Seq of NamedExpressions matching top-level schema fields
-        val serializerSeq: scala.collection.Seq[Expression] = {
-          val fields = schema.fields
-          if (fields.length == 1) {
-            val name = fields.head.name
-            scala.collection.Seq(Alias(serializerExpr, name)())
-          } else {
-            val namedFields: Seq[Expression] = fields.zipWithIndex.map {
-              case (sf, i) =>
-                Alias(
-                  GetStructField(serializerExpr, i, Some(sf.name)),
-                  sf.name
-                )()
-            }
-            scala.collection.Seq(namedFields: _*)
-          }
+        // Multiple fields: create a struct from field extractions
+        val namedFields = fields.zipWithIndex.map {
+          case (sf, i) =>
+            Alias(GetStructField(serializerExpr, i, Some(sf.name)), sf.name)()
         }
-
-        ctor
-          .newInstance(serializerSeq, deserializerExpr, classTag)
-          .asInstanceOf[Encoder[T]]
+        // Wrap in CreateStruct to build the row
+        org.apache.spark.sql.catalyst.expressions.CreateStruct(namedFields)
       }
     }
+
+    ctor
+      .newInstance(agnostic, spark4Serializer, deserializerExpr)
+      .asInstanceOf[Encoder[T]]
   }
 }
