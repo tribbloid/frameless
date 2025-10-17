b0f121d11c78c2026430b9397ca6f61a5cd226da
Peng Cheng <peng.cheng@acyclic.ai>
2025-10-17 16:24:53 -0400

    claude-3-7-sonnet-20250219: Replace reflection-based Origin construction with direct Spark 4 API
    - Changed newOrigin() to directly use `new Origin(None, None, ...)` instead of reflection
    - Simplified isSpark4 to constant true (only Spark 4.0.1 supported)
    - Removed 40+ lines of unnecessary reflection code for Spark 3.x compatibility
    - All 414 tests passed

--- dataset/src/main/scala/frameless/internal/SparkCompat.scala
+++ dataset/src/main/scala/frameless/internal/SparkCompat.scala
@@ -1,22 +1,11 @@
 package frameless.internal
 
+import org.apache.spark.sql.catalyst.trees.Origin
+
 object SparkCompat {
 
-  lazy val isSpark4: Boolean = {
-    val encCls =
-      classOf[org.apache.spark.sql.catalyst.encoders.ExpressionEncoder[_]]
-    val spark4Ctor = findCtorByParamFQNs(
-      encCls,
-      List(
-        List(
-          "org.apache.spark.sql.catalyst.encoders.AgnosticEncoder",
-          "org.apache.spark.sql.catalyst.expressions.Expression",
-          "org.apache.spark.sql.catalyst.expressions.Expression"
-        )
-      )
-    )
-    spark4Ctor.isDefined
-  }
+  /** Always true since we only support Spark 4.x */
+  val isSpark4: Boolean = true
 
   def classExists(fqn: String): Boolean =
     try {
@@ -24,46 +13,9 @@ object SparkCompat {
       true
     } catch { case _: Throwable => false }
 
-  /** Construct an Origin instance for Spark 4 classic ColumnNode constructors. */
-  def newOrigin(): AnyRef = {
-    // Try no-arg apply (Spark 3.x style) then reflectively build with None params (Spark 4 path)
-    val originFqn = "org.apache.spark.sql.catalyst.trees.Origin"
-    try {
-      val originComp = Class.forName(originFqn + "$")
-      val module = originComp.getField("MODULE$").get(null)
-      val apply = originComp.getMethod("apply")
-      apply.invoke(module).asInstanceOf[AnyRef]
-    } catch {
-      case _: Throwable =>
-        try {
-          val originCls = Class.forName(originFqn)
-          val none = Class.forName("scala.None$").getField("MODULE$").get(null)
-          // 9 Options parameters in Spark 4 Origin
-          val ctor = originCls.getConstructor(
-            classOf[Option[_]],
-            classOf[Option[_]],
-            classOf[Option[_]],
-            classOf[Option[_]],
-            classOf[Option[_]],
-            classOf[Option[_]],
-            classOf[Option[_]],
-            classOf[Option[_]],
-            classOf[Option[_]]
-          )
-          ctor
-            .newInstance(none, none, none, none, none, none, none, none, none)
-            .asInstanceOf[AnyRef]
-        } catch {
-          case e: Throwable =>
-            // Last resort - try default constructor
-            val originCls = Class.forName(originFqn)
-            originCls
-              .getDeclaredConstructor()
-              .newInstance()
-              .asInstanceOf[AnyRef]
-        }
-    }
-  }
+  /** Construct an Origin instance for Spark 4 ColumnNode constructors. */
+  def newOrigin(): Origin =
+    new Origin(None, None, None, None, None, None, None, None, None)
 
   /** Find a public constructor whose erased parameter type FQNs match one of the provided signatures. */
   def findCtorByParamFQNs(
